ba6900042f3f356872ce8ed816454c642de00986
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-05-14 13:59:42 -0500

    feat: fetch data and catch any errors

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,15 @@ import Header from './components/header'
 import SpeciesGrid from './components/species-grid/index'
 import { useState, useEffect } from 'react'
 
+const options = {
+  method: 'GET',
+
+  // `headers` notify the API that we want JSON data
+  headers: {
+    accept: 'application/json',
+  },
+}
+
 export default function App() {
   return (
     <main>
